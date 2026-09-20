28d859b224a85da30790e4d85fbe1e449dc79420
Umang somjiyani <umangsomjiyani36@gmail.com>
2026-09-20 17:57:03 +0530

    cors for the server, added the netlify link

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,11 @@ const contactRoutes = require("./routes/contact");
 
 const app = express();
 
-const allowedOrigins = ["http://localhost:3000", "http://127.0.0.1:5173"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://127.0.0.1:5173",
+  "https://codefolios.netlify.app",
+];
 
 app.use(
   cors({
